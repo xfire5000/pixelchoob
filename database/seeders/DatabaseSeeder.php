3e403a75d7dc4e4bb5ca88c55a58bf13a3cfb7db
Ali Zare <xfire5000@gmail.com>
2024-07-17 22:31:27 +0330

    modified DatabaseSeeder.php and package-lock.json

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RoleSeeder::class,
+            SettingSeed::class,
         ]);
         // if (\App\Models\User::count() < 10) {
         //     \App\Models\User::factory(10)->create();
